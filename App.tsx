24d0b2609e62309240e5bf0bb218249e6456a21a
Bhupendra Negi <bhupendra1011@gmail.com>
2024-05-28 18:42:05 +0530

    added permission

--- App.tsx
+++ App.tsx
@@ -6,12 +6,34 @@
  */
 
 import React from 'react';
-import {SafeAreaView, useWindowDimensions} from 'react-native';
+import {
+  SafeAreaView,
+  useWindowDimensions,
+  PermissionsAndroid,
+  Platform,
+} from 'react-native';
 import {WebView} from 'react-native-webview';
 
 function App(): React.JSX.Element {
   const {height, width} = useWindowDimensions();
   const DEPLOYED_APP_URL = 'https://conferencing.agora.io';
+
+  const requestPermissions = async () => {
+    try {
+      if (Platform.OS === 'android') {
+        await PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.CAMERA,
+          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        ]);
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  React.useEffect(() => {
+    requestPermissions();
+  }, []);
 
   return (
     <SafeAreaView style={{flex: 1}}>
